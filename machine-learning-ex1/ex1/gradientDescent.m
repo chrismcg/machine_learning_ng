cb072fe483515e2e00db6479a8636ad2283a79c7
Chris McGrath <chris@chrismcg.com>
2017-09-21 01:16:13 +0100

    gradient descent

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -17,11 +17,9 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
+  h = X * theta;
+  errors = h - y;
+  theta = theta - (alpha * ((X' * errors) / m));
 
     % ============================================================
 
