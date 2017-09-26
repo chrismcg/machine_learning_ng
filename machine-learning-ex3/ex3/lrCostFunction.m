52d0eb8d0eab48f26087d20bfe4517c720e45ddc
Chris McGrath <chris@chrismcg.com>
2017-09-26 22:48:38 +0100

    Implement lrCostFunction

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -38,7 +38,17 @@ grad = zeros(size(theta));
 
 
 
+h = sigmoid(X * theta);
+regularization = (lambda / (2 * m)) * sum(theta(2:end) .^ 2);
 
+J = (((-y' * log(h)) - ((1 - y') * log(1 - h))) / m) + regularization;
+
+grad = (X' * (h - y)) / m;
+
+temp = theta;
+temp(1) = 0;
+
+grad = grad + ((lambda / m) * temp);
 
 
 
